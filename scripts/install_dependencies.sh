4832d487f12348711594b118504d0bc127d3aa8e
utkarsh-myntra <utkarsh.saxena1@myntra.com>
2022-01-19 15:08:50 +0530

    debugging

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 cd /tmp/
+
+curl --silent --location https://rpm.nodesource.com/setup_17.x | bash -
+yum install -y gcc-c++ make
+yum install -y nodejs
+
+node -v
+
+npm install -g pm2
+npm install
