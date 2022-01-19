6433c2d2d12e7d8af2e597b2957177307e3bcf66
utkarsh-myntra <utkarsh.saxena1@myntra.com>
2022-01-19 13:56:56 +0530

    remove dependencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
 cd /tmp/
-
-curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
-yum install -y gcc-c++ make
-yum install -y nodejs
-
-npm install -g pm2
-npm install
